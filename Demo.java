2be7349455a9d42db01ffe14917857e709deb757
Connor Khin <khincc@appstate.edu>
2023-01-27 17:51:08 -0500

    created Game abstract class

--- Demo.java
+++ Demo.java
@@ -1,3 +1,15 @@
+/**
+ *  Does something
+ *  @author Connor Khin
+ *  @version 1/27/23
+ */
+import java.util.Scanner
 public class Demo {
-
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in)
+                
+        while
+        System.out.println("Would you like to play a game? (y/n)");
+        
+    }
 }
