dfcdf691df046b2b6a7718933cacf33e50168065
Connor Khin <khincc@appstate.edu>
2023-01-28 01:34:01 -0500

    changed selection and added game to Demo

--- Demo.java
+++ Demo.java
@@ -8,19 +8,40 @@ public class Demo {
     public static void main(String[] args) {
         Scanner kb = new Scanner(System.in)
         String response;
-        
+        char c;
+        boolean invalidAns = true;
+
         do{
             System.out.println("Would you like to play a game? (y/n)");
             reponse = kb.nextLine();
-        }while(validAns(response))
-    }
-    
-    public static boolean validAns(String s) {
-        char c = s.charAt(0);
-        boolean valid = false;
-        if(c == 'y' || c == 'n' || c == 'Y' || c == 'N') {
-            valid = true;
+            c = response.charAt(0)
+            switch(c) {
+                case y:
+                case Y:
+                    c = 'y';
+                case n:
+                case N:
+                    invalidAns = false;
+                    break;
+                default:
+                    System.out.println("Please respond with yes or no.");
+            }
+        }while(validAns)
+
+        Game game;
+        if(c == 'y') {
+            System.out.println("Good, let's play hangman.")
+            game = new hangman();
+        }else {
+            System.out.println("Ok, here's some work to do instead.");
+            game = new addition();
         }
-        return valid;
+
+        boolean unfinished = true;
+        game.output();
+        while(unfinished) {
+            game.input();
+            unfinished = game.output();
+        }
     }
 }
