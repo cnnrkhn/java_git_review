f789a8128154b0be7845e8754f8e32cd501ae448
Connor Khin <khincc@appstate.edu>
2023-01-27 22:23:49 -0500

    added game selection

--- Demo.java
+++ Demo.java
@@ -7,9 +7,20 @@ import java.util.Scanner
 public class Demo {
     public static void main(String[] args) {
         Scanner kb = new Scanner(System.in)
-                
-        while
-        System.out.println("Would you like to play a game? (y/n)");
+        String response;
         
+        do{
+            System.out.println("Would you like to play a game? (y/n)");
+            reponse = kb.nextLine();
+        }while(validAns(response))
+    }
+    
+    public static boolean validAns(String s) {
+        char c = s.charAt(0);
+        boolean valid = false;
+        if(c == 'y' || c == 'n' || c == 'Y' || c == 'N') {
+            valid = true;
+        }
+        return valid;
     }
 }
